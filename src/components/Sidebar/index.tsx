8fa5afa00bbc0eab21dda5b72ff2b4b1566b455c
Mélody De Sousa <melody@beamxp.com>
2024-09-09 22:08:14 +0200

    fix margin

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -26,12 +26,12 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-[#2000ad] duration-300 ease-linear lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
     >
-      <div className="flex items-center justify-between gap-2 px-10 mt-6 py-5.5 lg:py-6.5">
+      <div className="flex items-center justify-between gap-2 px-10 sm:mt-8 mt-10 py-5.5 lg:py-6.5">
         <NavLink onClick={() => setSidebarOpen(!sidebarOpen)} to={'/'} ><img src={Logo} alt="Logo" /></NavLink>
       </div>
 
       <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-        <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
+        <nav className="py-4 px-4 lg:px-6">
           <div>
             <ul className="mb-6 flex flex-col gap-1.5">
               <>
